c632bc6d7287da7a53335d0559fcbe017ef96043
huutrungrimp <67027279+huutrungrimp@users.noreply.github.com>
2022-12-05 09:49:49 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -148,7 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CORS_ALLOWED_ORIGINS = [
     'https://localhost:3000',
     'http://localhost:3000',
-    'https://frontlogger.herokuapp.com'
+    'https://financelog.up.railway.app'
 ]
 
 
