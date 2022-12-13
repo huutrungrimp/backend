5c1749c44a27851e2eeec814cae43a5a794f418c
huutrungrimp <huutrungrimp@gmail.com>
2022-12-13 07:32:50 -0500

    task_rate

--- tasks/models.py
+++ tasks/models.py
@@ -11,6 +11,12 @@ class Task(models.Model):
     date_time_end = models.DateTimeField()
     hours = models.FloatField(null=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    task_rate = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    def task_pay(self):
+        amount = self.hours*self.task_pay
+        return amount
+
