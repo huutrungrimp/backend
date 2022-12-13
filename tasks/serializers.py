cda316de3dbd8d6fdf6abbe0fdff31fdea707018
huutrungrimp <huutrungrimp@gmail.com>
2022-12-13 11:02:32 -0500

    task_pay

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -9,4 +9,4 @@ class TaskSerializer(serializers.ModelSerializer):
     customer = CustomerSerializer(read_only=True)
     class Meta:
         model = Task
-        fields = ['id', 'user', 'customer', 'title','isCompleted', 'date_time_start', 'date_time_end', 'hours', 'task_rate']
+        fields = ['id', 'user', 'customer', 'title','isCompleted', 'date_time_start', 'date_time_end', 'hours', 'task_rate', 'task_pay']
