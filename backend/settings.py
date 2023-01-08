e775ea8beb4d18bd8ecefb19cf59ca31bbd49928
huutrungrimp <huutrungrimp@gmail.com>
2023-01-08 14:09:49 -0500

    timezone

--- backend/settings.py
+++ backend/settings.py
@@ -128,13 +128,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Canada/eastern'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 
